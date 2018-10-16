21e6023bc1e2df047981de198da8b19140aff281
mi nombre <miemail@gmail.com>
2018-10-16 16:45:47 -0300

    ejercicio 1

--- ejemplo_aux3/app_de_IN3501/views.py
+++ ejemplo_aux3/app_de_IN3501/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'app_de_IN3501/index.html')
+	contexto = {"saludo":"hola", "Cargo_1": "AuxiliaR", "Cargo_2": "Ayudante", "Cargo_3": "Profesor"}
+	return render(request, 'app_de_IN3501/index.html',contexto)
 
 
 def welcome(request):
-    return render(request, 'app_de_IN3501/bienvenida.html')
+	return render(request, 'app_de_IN3501/bienvenida.html')
 
 
 def tarea(request):
-    return render(request, 'app_de_IN3501/tarea.html')
+	return render(request, 'app_de_IN3501/tarea.html')
